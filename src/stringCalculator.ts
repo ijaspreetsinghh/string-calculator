8e5561a803fa201875f9fde066b85d28e5e1a233
Jaspreet Singh <44422128+ijaspreetsinghh@users.noreply.github.com>
2024-06-26 16:24:03 +0530

    return number itself for single input

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,5 +2,11 @@ export function Add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
-  return 1;
+
+  const numberArray = numbers.split(",").map(Number);
+
+  if (numberArray.length === 1) {
+    return numberArray[0];
+  }
+  return 0;
 }
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(Add("")).toBe(0);
   });
+
+  test("should return the number itself for a single number", () => {
+    expect(Add("1")).toBe(1);
+  });
 });
